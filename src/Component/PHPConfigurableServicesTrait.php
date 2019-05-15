5b9bbb4fbd06e6857da5dfee8e282c7a3dc12ddf
Leo <losoviz@gmail.com>
2019-05-15 23:13:55 +0800

    Enable configuration by PHP

---
+++ src/Component/PHPConfigurableServicesTrait.php
@@ -0,0 +1,17 @@
+<?php
+namespace PoP\Root\Component;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use PoP\Root\Container\ContainerBuilderFactory;
+
+trait PHPConfigurableServicesTrait
+{
+    public static function initPHPServiceConfiguration(string $componentDir)
+    {
+        // Initialize the ContainerBuilder with this component's service implementations
+        $containerBuilder = ContainerBuilderFactory::getInstance();
+        $loader = new PhpFileLoader($containerBuilder, new FileLocator($componentDir));
+        $loader->load('config/services.php');
+    }
+}
