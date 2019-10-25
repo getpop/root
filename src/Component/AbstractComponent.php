8702855c377989425b66514c4d984b9e3937a459
Leo <losoviz@gmail.com>
2019-10-25 17:05:01 +0800

    Moved $enabled down from the AbstractComponent

--- src/Component/AbstractComponent.php
+++ src/Component/AbstractComponent.php
@@ -8,33 +8,12 @@ use PoP\Root\Managers\ComponentManager;
 abstract class AbstractComponent
 {
     /**
-     * Indicate if the component is enabled or not. It allows to disable it through environment variables
-     *
-     * @var boolean
-     */
-    protected static $enabled;
-
-    /**
      * Initialize services
      */
     public static function init()
     {
-        // Define if the component is enabled
-        self::initEnabled();
-        if (self::isEnabled()) {
-            // Register itself in the Manager
-            ComponentManager::register(get_called_class());
-        }
-    }
-
-    protected static function initEnabled()
-    {
-        self::$enabled = true;
-    }
-
-    public static function isEnabled()
-    {
-        return self::$enabled;
+        // Register itself in the Manager
+        ComponentManager::register(get_called_class());
     }
 
     /**
