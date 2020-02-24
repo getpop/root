c9e732925fa6f27fe7798644d732c1215b28dbad
Leo <losoviz@gmail.com>
2020-02-24 14:24:55 +0800

    Enable to load services.yaml under subpath

--- src/Component/YAMLServicesTrait.php
+++ src/Component/YAMLServicesTrait.php
@@ -7,14 +7,15 @@ use PoP\Root\Container\ContainerBuilderFactory;
 
 trait YAMLServicesTrait
 {
-    public static function initYAMLServices(string $componentDir)
+    public static function initYAMLServices(string $componentDir, string $configPath = '', string $fileName = 'services.yaml')
     {
         // First check if the container has been cached. If so, do nothing
         if (!ContainerBuilderFactory::isCached()) {
             // Initialize the ContainerBuilder with this component's service implementations
             $containerBuilder = ContainerBuilderFactory::getInstance();
-            $loader = new YamlFileLoader($containerBuilder, new FileLocator($componentDir));
-            $loader->load('config/services.yaml');
+            $componentPath = $componentDir.'/config'.($configPath ? '/'. trim($configPath, '/') : '');
+            $loader = new YamlFileLoader($containerBuilder, new FileLocator($componentPath));
+            $loader->load($fileName);
         }
     }
 }
