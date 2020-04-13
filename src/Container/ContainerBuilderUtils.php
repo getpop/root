65d29f3c57d531dc53b93e520f2881fac6db5577
Leo <losoviz@gmail.com>
2020-04-13 19:19:29 +0800

    Added declare(strict_types=1);

--- src/Container/ContainerBuilderUtils.php
+++ src/Container/ContainerBuilderUtils.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\Root\Container;
 
 use PoP\Root\Container\ContainerBuilderFactory;
--- src/Dotenv/DotenvBuilderFactory.php
+++ src/Dotenv/DotenvBuilderFactory.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\Root\Dotenv;
 
 use Symfony\Component\Dotenv\Dotenv;
