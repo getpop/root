72fd6372da7e6d4aeee7d4aa128294da169015d8
Leonardo Losoviz <leo@getpop.org>
2020-05-26 17:30:16 +0800

    Ignore classes to initialize

--- src/ComponentLoader.php
+++ src/ComponentLoader.php
@@ -15,33 +15,52 @@ class ComponentLoader
     protected static $initializedClasses = [];
 
     /**
-     * Initialize the component
+     * Initialize the PoP components
+     *
+     * @param array $componentClasses List of `Component` class to initialize
+     * @param array $ignoreComponentClasses List of `Component` class to not initialize
+     * @return void
      */
-    public static function initializeComponents(array $componentClasses): void
-    {
+    public static function initializeComponents(
+        array $componentClasses,
+        array $ignoreComponentClasses = []
+    ): void {
+        /**
+         * If any component class is on the ignore list, or has already been initialized,
+         * then do not initialize it
+         */
+        $componentClasses = array_values(array_diff(
+            $componentClasses,
+            $ignoreComponentClasses,
+            self::$initializedClasses
+        ));
         foreach ($componentClasses as $componentClass) {
-            if (!in_array($componentClass, self::$initializedClasses)) {
-                self::$initializedClasses[] = $componentClass;
+            self::$initializedClasses[] = $componentClass;
 
-                // Initialize all depended-upon PoP components
-                self::initializeComponents($componentClass::getDependedComponentClasses());
+            // Initialize all depended-upon PoP components
+            self::initializeComponents(
+                $componentClass::getDependedComponentClasses(),
+                $ignoreComponentClasses
+            );
 
-                // Initialize all depended-upon PoP conditional components, if they are installed
-                self::initializeComponents(array_map(
-                    'class_exists',
-                    $componentClass::getDependedConditionalComponentClasses()
-                ));
+            // Initialize all depended-upon PoP conditional components, if they are installed
+            self::initializeComponents(
+                array_filter(
+                    $componentClass::getDependedConditionalComponentClasses(),
+                    'class_exists'
+                ),
+                $ignoreComponentClasses
+            );
 
-                // Temporary solution until migrated:
-                // Initialize all depended-upon migration plugins
-                foreach ($componentClass::getDependedMigrationPlugins() as $migrationPlugin) {
-                    // All migration plugins go under /getpop, and have `initialize.php` as entry point
-                    require_once dirname(dirname(dirname(__DIR__))) . '/getpop/' . $migrationPlugin . '/initialize.php';
-                }
-
-                // Initialize the component
-                $componentClass::initialize();
+            // Temporary solution until migrated:
+            // Initialize all depended-upon migration plugins
+            foreach ($componentClass::getDependedMigrationPlugins() as $migrationPlugin) {
+                // All migration plugins go under /getpop, and have `initialize.php` as entry point
+                require_once dirname(dirname(dirname(__DIR__))) . '/getpop/' . $migrationPlugin . '/initialize.php';
             }
+
+            // Initialize the component
+            $componentClass::initialize();
         }
     }
 }
