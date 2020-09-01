c637199f5c093549204ac1d880cf264fe747fd49
Leonardo Losoviz <leo@getpop.org>
2020-09-01 18:38:05 +0800

    Run Scrutinizer with PHP 7.4

--- src/Component.php
+++ src/Component.php
@@ -15,6 +15,11 @@ class Component extends AbstractComponent
 {
     // const VERSION = '0.1.0';
 
+    /**
+     * Classes from PoP components that must be initialized before this component
+     *
+     * @return array<string>
+     */
     public static function getDependedComponentClasses(): array
     {
         return [];
